d0db0614ccfd87076dd63855f300bc31867e58e8
Francisco Paulo <francisco.paulo@datadoghq.com>
2023-11-28 14:50:07 +0000

    Was about to start the work on the DB semantic definitions, but handing this off to Andrew.

--- semantic-core/gen_semantic_defs.py
+++ semantic-core/gen_semantic_defs.py
@@ -113,8 +113,18 @@ IpAddress = Annotated[
     ),
 ]
 
-# Semantic Models
+DbSystem = Annotated[
+    str,
+    Field(
+        description="""
+        An identifier for the database management system (DBMS) product being used.""",
+        examples=["mysql", "postgresql"],
+        pattern=r"^(adabas|buntdb|cache|cassandra|cloudscape|cockroachdb|coldfusion|consul|cosmosdb|couchbase|couchdb|db2|derby|dynamodb|edb|elasticsearch|eloquent|filemaker|firebird|firstsql|geode|h2|hanadb|hbase|hive|hsqldb|informix|ingres|instantdb|interbase|leveldb|mariadb|maxdb|memcached|mongodb|mssql|mysql|neo4j|netezza|opensearch|oracle|other_sql|pervasive|pointbase|postgresql|presto|progress|redis|redshift|snowflake|sqlite|sybase|teradata|vertica)$",
+        json_schema_extra={"is_sensitive": False},
+    ),
+]
 
+# Semantic Models
 
 class IntakeResolvedHttpSpan(BaseModel):
     """
