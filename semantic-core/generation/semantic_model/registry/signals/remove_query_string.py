b152f7f9a768d35a4c0862db3a338fe11ef8fc31
Francisco Paulo <123977764+fpddog@users.noreply.github.com>
2024-01-12 09:42:16 +0000

    Update semantic-core/generation/semantic_model/registry/signals/remove_query_string.py
    Co-authored-by: Iñigo López de Heredia <inigo.lheredia@gmail.com>

--- semantic-core/generation/semantic_model/registry/signals/remove_query_string.py
+++ semantic-core/generation/semantic_model/registry/signals/remove_query_string.py
@@ -3,7 +3,7 @@ from .util import signal
 RemoveQueryString = signal(
     signal_type=bool,
     description="""
-    True if the agent that sent a payload containing an http utl is configured to remove the query string from the url before sending it to the backend.
+    True if the agent that sent a payload containing an http url is configured to remove the query string from the url before sending it to the backend.
     See: https://docs.datadoghq.com/tracing/configure_data_security/?tab=http
     """,
 )
