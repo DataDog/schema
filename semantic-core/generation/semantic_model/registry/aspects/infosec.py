69371086645d2cf4ef8a9d4d7059f92102c39bbd
Francisco Paulo <francisco.paulo@datadoghq.com>
2024-06-05 10:04:03 +0100

    Removing the deprecated schema for the Semantic Core

--- semantic-core/generation/semantic_model/registry/aspects/infosec.py
+++
@@ -1,7 +0,0 @@
-from .util import aspect
-
-InfoSec = aspect(
-    id="infosec",
-    owner="trust_and_safety",
-    description="This aspect relates to infosec concerns",
-)
--- semantic-core/generation/semantic_model/registry/signals/remove_query_string.py
+++
@@ -1,9 +0,0 @@
-from .util import signal
-
-RemoveQueryString = signal(
-    signal_type=bool,
-    description="""
-    True if the agent that sent a payload containing an http url is configured to remove the query string from the url before sending it to the backend.
-    See: https://docs.datadoghq.com/tracing/configure_data_security/?tab=http
-    """,
-)
